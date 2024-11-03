28b35f3c33b6228b96e56b508d7b8362f662da2c
arseeenyyy <rubtsov113@mail.ru>
2024-11-03 14:54:31 +0300

    complete validation n business logic

--- lab3/src/main/java/com/github/arseeenyyy/utils/Checker.java
+++ lab3/src/main/java/com/github/arseeenyyy/utils/Checker.java
@@ -1,5 +1,18 @@
 package com.github.arseeenyyy.utils;
 
 public class Checker {
-    
+
+    public static boolean isHit(float x, float y, float r) {
+        return inSquare(x, y, r) || inTriangle(x, y, r) || inCircle(x, y, r);
+    }
+
+    public static boolean inSquare(float x, float y, float r) {
+        return x <= r && -y <= r;
+    }
+    public static boolean inTriangle(float x, float y, float r) {
+        return x <= 0 && y >= 0 && -x <= r && y <= r && y <= r - x;
+    }
+    public static boolean inCircle(float x, float y, float r) {
+        return x <= 0 && y <= 0 && -x <= r && -y <= r && (Math.pow(x, 2) + Math.pow(y, 2)) <= Math.pow(r, 2);
+    }
 }
--- lab3/src/main/java/com/github/arseeenyyy/utils/Validator.java
+++ lab3/src/main/java/com/github/arseeenyyy/utils/Validator.java
@@ -1,5 +1,13 @@
 package com.github.arseeenyyy.utils;
 
 public class Validator {
-    
+    public static boolean validateX(float x) {
+        return x >= -5.0f && x <= 5.0f;
+    }
+    public static boolean validateY(float y) {
+        return y >= -5.0f && y <= 5.0f;
+    }
+    public static boolean validateR(float r) {
+        return r >= 1 && r <= 3 && (r * 2) % 10 == 0;
+    }
 }
